5d9a69a7957e5e588876d1aae4e2d84b4a3a71d5
Mario Lopez <mariodlopez17@gmail.com>
2022-11-15 13:09:43 -0600

    antenimientos actualizacion

---
+++ Codigo/Modulos/Produccion/CapaVista/Clientes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace CapaVistaProduccion
+{
+    public partial class Clientes : Form
+    {
+        string connectionString = @"Server=localhost;Database=colchoneria;Uid=root;Pwd=root";
+        int clientes = 0;
+        public Clientes()
+        {
+            InitializeComponent();
+        }
+
+        void GridFill()
+        {
+            using (MySqlConnection mysqlCon = new MySqlConnection(connectionString))
+            {
+                mysqlCon.Open();
+                MySqlDataAdapter sqlDa = new MySqlDataAdapter("clientes_viewall", mysqlCon);
+                sqlDa.SelectCommand.CommandType = CommandType.StoredProcedure;
+                DataTable dtblhab = new DataTable();
+                sqlDa.Fill(dtblhab);
+                dataGridView1.DataSource = dtblhab;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GridFill();
+        }
+    }
+}
---
+++ Codigo/Modulos/Produccion/CapaVista/ver_reserva.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVistaProduccion
+{
+    public partial class ver_reserva : Form
+    {
+        public ver_reserva()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+            NavegadorVista.Navegador.idApp = "5101";
+            TextBox[] Grupotextbox = { T1, T2, T3, T4, T5, T6, T7 };
+            TextBox[] Idtextbox = { T1, T2 };
+            navegador1.textbox = Grupotextbox;
+            navegador1.tabla = dataGridView1;
+            navegador1.textboxi = Idtextbox;
+            navegador1.actual = this;
+            navegador1.cargar(dataGridView1, Grupotextbox, "colchoneria");
+        }
+    }
+}
